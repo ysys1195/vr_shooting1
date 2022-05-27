6deb3b98177cf2c0b410eeb45c64cb03f978a0e5
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2022-05-27 23:37:23 +0900

    一旦コミット

--- Assets/Nokobot/Modern Guns - Handgun/_Demo Assets/SimpleShoot.cs
+++ Assets/Nokobot/Modern Guns - Handgun/_Demo Assets/SimpleShoot.cs
@@ -32,11 +32,16 @@ public class SimpleShoot : MonoBehaviour
 
     void Update()
     {
-        //If you want a different input, change it here
-        if (Input.GetButtonDown("Fire1"))
+        // //If you want a different input, change it here
+        // if (Input.GetButtonDown("Fire1"))
+        // {
+        //     //Calls animation on the gun that has the relevant animation events that will fire
+        //     gunAnimator.SetTrigger("Fire");
+        // }
+
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
         {
-            //Calls animation on the gun that has the relevant animation events that will fire
-            gunAnimator.SetTrigger("Fire");
+            GetComponent<Animator>().SetTrigger("Fire");
         }
     }
 
